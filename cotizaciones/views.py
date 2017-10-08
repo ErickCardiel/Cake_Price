18f92b1a3ad787653c608cfcbe7269625dc4b5b7
Adel Leon <adel.leon@uabc.edu.mx>
2017-10-08 15:49:21 -0700

    Realizar prueba GET para mandar modelo en JSON

--- cotizaciones/views.py
+++ cotizaciones/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from cotizaciones.models import Tamano
+from cotizaciones.serializers import TamanoSerializer
 
-def index(request):
-    return HttpResponse("<h1> Cotizaciones </h1>")
-
+@csrf_exempt
+def lista_tamanos(request):
+    if request.method == 'GET':
+        tamanos = Tamano.objects.all()
+        serializer = TamanoSerializer(tamanos, many=True)
+        return JsonResponse(serializer.data, safe=False)
