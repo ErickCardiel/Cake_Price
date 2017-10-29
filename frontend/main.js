dc3aeb242491d4913efd3b807ce88148397bf4ce
Adel Leon <adel.leon@uabc.edu.mx>
2017-10-29 14:08:11 -0700

    Reescribir función datosPastel con ES6 Fetch API
    Las funciones de jQuery fueron comentadas

--- frontend/main.js
+++ frontend/main.js
@@ -1,45 +1,42 @@
 var objetoPastel;
 
-function datosPastel(){
-    $.ajax("http://localhost:8000/cotizaciones/api/all/",
-        {
-            method:"GET"
-        }
-    ).then(function(res){
-            console.log(res);
-            objetoPastel = res;
-        },
-        function(err){
-            console.error(err);
-        }
-    );
-}
-
-function desplegarDatos(){
-    $.each(objetoPastel, function( index, array ) {
-        $.each(array, function(object, arrayObject){
-           console.log(arrayObject.nombre);
-           $("#lista").append(arrayObject.nombre);
-        });
+function datosPastel() {
+    fetch('http://localhost:8000/cotizaciones/api/all/').then(function (response) {
+        return response.json();
+    }).then(function (myjson) {
+        objetoPastel = myjson;
     });
 }
 
-function desplegarAditivos(){
-    $.each(objetoPastel.AditivosPan, function (object, arrayObject) {
-        $("#lAdiciones").append("<li>"+arrayObject.nombre+"</li>");
-    })
-}
+// function desplegarDatos(){
+//     $.each(objetoPastel, function( index, array ) {
+//         $.each(array, function(object, arrayObject){
+//            console.log(arrayObject.nombre);
+//            $("#lista").append(arrayObject.nombre);
+//         });
+//     });
+// }
 
-function desplegarTamano(){
-    $.each(objetoPastel.Tamano, function (object, arrayObject) {
-        $("#lTamano").append("<li>"+arrayObject.nombre+"</li>");
-    })
-}
+// function desplegarAditivos(){
+//     $.each(objetoPastel.AditivosPan, function (object, arrayObject) {
+//         $("#lAdiciones").append("<li>"+arrayObject.nombre+"</li>");
+//     })
+// }
+//
+// function desplegarTamano(){
+//     $.each(objetoPastel.Tamano, function (object, arrayObject) {
+//         $("#lTamano").append("<li>"+arrayObject.nombre+"</li>");
+//     })
+// }
 
-$(document).ready(function(){
+function init(){
     console.log("javascript loaded");
     datosPastel();
-    desplegarDatos();
-    desplegarTamano();
-    desplegarAditivos();
-});
+    console.log(objetoPastel);
+    // desplegarTamano();
+    // desplegarAditivos();
+}
+
+window.onload = function() {
+    init();
+};
